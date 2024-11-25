6a633f8b66ee96623cb2c11cee32f40628f466fc
Arafat Patwary <farabipsk@gmail.com>
2024-11-25 08:57:59 +0600

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Make sure BrowserRouter is imported
 import "./App.css";
 import NavBar from "./component/NavBar/NavBar";
 import HomePage from "./component/Template/HomePage";
+import Post from "./component/Template/Post";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/post" element={<Post />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
